530f7b64ef2cd65ce0ca4c29feae1fd38145c7e6
menaalhassan <MY_NAME@example.com>
2021-10-21 09:55:32 +1100

    completed

---
+++ 01hello.py
@@ -0,0 +1,8 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    01HelloWorld                                |")
+print("|    Name : Menaal Hassan                        |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+print("Hello World")
