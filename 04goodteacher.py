091354dc0aac3b3fca11207c0f68dcd6e335e565
menaalhassan <MY_NAME@example.com>
2021-10-21 22:35:59 +1100

    done

---
+++ 04goodteacher.py
@@ -0,0 +1,12 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    04GoodTeacher                               |")
+print("|    Name : Menaal Hassan                        |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+name = input("Teacher's name (try Mr Horan) ")
+if name == "Mr Horan":
+    print("You are lucky, he is a great teacher.")
+else:
+    print(name,"is an ok teacher")
