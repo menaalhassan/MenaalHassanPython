90d6660343f351397cba10f4270b946377ee8400
menaalhassan <MY_NAME@example.com>
2021-10-28 10:56:28 +1100

    done

--- 06whileloop.py
+++ 06whileloop.py
@@ -1,14 +1,19 @@
-print(" ------------------------------------------------") 
+print(' -' + '-'*(46) + '-')
 print("|                                                |")
 print("|    06whileLoop                                 |")
-print("|    Name : Menaal Hassan                        |") 
+print("|    Name : Aditi                                |")
 print("|    Version : 01                                |")
 print("|                                                |")
-print(" ------------------------------------------------")
-ans = input("What is the name of this class? ")
-while ans != ("IST"):
-    print("Not correct - try again")
-    ans = input("What is the name of this class? ")
-    continue
-else:
-    print("\nCongratulations!!")
+print(' -' + '-'*(46) + '-')
+i = 0
+while i == 0:
+    name = input("What is the name of this subject ")
+    if name == "IST":
+        print("")
+        print("")
+        print(" Congratulations!!")
+        print("")
+        print("")
+        break
+    else:
+        print("Not Correct - try again")
