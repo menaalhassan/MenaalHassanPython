0f374f968ae0e217fa9469c7323e5335eb6267e9
menaalhassan <MY_NAME@example.com>
2021-11-04 20:56:28 +1100

    done

---
+++ hangman.py
@@ -0,0 +1,31 @@
+from random import choice
+
+words = ["hello", "there", "school", "food"]
+
+chosen_word = choice(words)
+
+#underscore is length of the chosen word
+displayed_word = '_' * len(chosen_word)
+
+print(displayed_word)
+
+TOTAL_ALLOWED_GUESS = 10
+
+for x in range(TOTAL_ALLOWED_GUESS):
+    guess_letter = input("Guess a letter: ")
+
+    if guess_letter in chosen_word:
+        offset = 0
+        while offset < len(chosen_word):
+            try:
+                i = chosen_word.index(guess_letter, offset)
+                offset = i + 1
+                displayed_word = displayed_word[:i] + guess_letter + displayed_word[i+1:]
+            except ValueError as e:
+                break    
+
+    print(displayed_word)
+
+    if '_' not in displayed_word:
+        print("Congratulations! YOU WON!")
+        break
